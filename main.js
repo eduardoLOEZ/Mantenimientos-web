ee23a5a274289ea46a5bcc2eca59b7cd40996a94
eduardoLOEZ <84247208+eduardoLOEZ@users.noreply.github.com>
2021-05-15 14:13:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards',{delay: 500});
+ScrollReveal().reveal('.cards-banner-one',{delay: 500});
+ScrollReveal().reveal('.card-banner-two',{delay: 500});
